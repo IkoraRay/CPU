97847b5f3e23e704021e28f08f900c70bd99371b
IkoraRay <luis.stein@hotmail.com>
2020-10-21 15:11:34 -0300

    Teste branch PC

---
+++ PC.vhd
@@ -0,0 +1,26 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY PC IS
+	PORT (
+			END_IN: IN STD_LOGIC_VECTOR(31 DOWNTO 0);
+			
+			END_OUT: OUT STD_LOGIC_VECTOR(31 DOWNTO 0);
+			Clock, PCWrite : IN STD_LOGIC;
+			);
+			END PC;
+			ARCHITECTURE Behavior OF PC IS
+			BEGIN
+			--Caso não funcione, adicionar signal
+					PROCESS (Clock, PCWrite)
+					BEGIN
+							IF Clock'EVENT AND PCWrite = '1' THEN
+							END_OUT <= END_IN;
+							END IF;
+					END PROCESS;
+			END Behavior;
+			
+							
+							
+				
+			
