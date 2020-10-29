07acfcb72e7de2e0a739364300b550e18f3f0f88
brunobannwart <bbannwart@yahoo.com.br>
2020-10-29 08:43:19 -0300

    Adição de sinais de controle, com base no state

--- Controle.vhd
+++ Controle.vhd
@@ -3,9 +3,17 @@ USE ieee.std_logic_signed.all;
 
 ENTITY Controle IS
 	PORT (
-		clk			: IN	STD_LOGIC;
-		instruction	: IN 	STD_LOGIC_VECTOR(15 DOWNTO 0);
-		OPCode		: OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+		clk				: IN	STD_LOGIC;
+		instruction		: IN 	STD_LOGIC_VECTOR(15 DOWNTO 0);
+		state_if			: IN	STD_LOGIC;
+		state_mem		: IN	STD_LOGIC;
+		state_reg		: IN	STD_LOGIC;
+		
+		OPCode		: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
+		MemWrite		: OUT	STD_LOGIC;
+		RegWrite		: OUT STD_LOGIC;
+		Resetn		: OUT STD_LOGIC;
+		PCWrite		: OUT STD_LOGIC
 	);
 END Controle;
 
@@ -28,6 +36,32 @@ BEGIN
 					WHEN "1011" =>  OPCode <= "11";
 					WHEN OTHERS =>  OPCode <= "XX";						
 			END CASE;
+			
+			Resetn 	<= '0';		
+			
+			IF state_if = '1' THEN
+				PCWrite <= '1';
+				
+			ELSE
+				PCWrite <= '0';
+				
+			END IF;
+				
+			if state_mem = '1' THEN
+				MemWrite <= '1';
+				
+			ELSE
+				MemWrite <= '0';
+				
+			END IF;
+				
+			if state_reg = '1' THEN
+				RegWrite <= '1';
+				
+			ELSE
+				RegWrite <= '0';
+				
+			END IF;
 			
 		END IF;
 	END PROCESS;
